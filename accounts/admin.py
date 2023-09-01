e36d4e44de2aa790b5ac8082659f242ef04543d9
Amir <atjmm1769@gmail.com>
2023-09-01 13:08:17 -0700

    add setings temp

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     add_form = UserCreationForm
     form = UserChangeForm
     list_display = ('email', 'username')
-
